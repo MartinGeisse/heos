88a9c923c0a803c223057b024b285a8a3c9a129d
Martin Geisse <martingeisse@googlemail.com>
2020-05-15 09:49:16 +0200

    refactor command parsing

--- src/heos/c/shell/shell.c
+++ src/heos/c/shell/shell.c
@@ -89,6 +89,7 @@ static int parseValue(const char *text, const shell_ValuePattern *pattern, void 
 
 static char *segments[16];
 static int segmentCount;
+static const shell_CommandPattern *commandPattern;
 
 static int splitSegments(char *commandLine) {
     segmentCount = 0;
@@ -119,6 +120,83 @@ static void echoSplitCommandLine(void) {
 	}
 	driver_console_println("");
 }
+
+// --------------------------------------------------------------------------------------------------------------------
+// command implementation API
+// --------------------------------------------------------------------------------------------------------------------
+
+void foo(void) {
+	// parse the command's arguments
+	int minimumRepetitions = (commandPattern->repeatedArgument == NULL ? 0 : commandPattern->repeatedArgumentMinimumRepetitions);
+	argumentCount = segmentCount - 1;
+	if (argumentCount < commandPattern->fixedArgumentCount + minimumRepetitions) {
+		driver_console_print("too few arguments for '");
+		driver_console_print(commandName);
+		driver_console_println("'. Usage:");
+		shell_printSynopsis(commandPattern);
+		driver_console_println("");
+		return;
+	}
+	if (commandPattern->repeatedArgument == NULL && argumentCount > commandPattern->fixedArgumentCount) {
+		driver_console_print("too many arguments for '");
+		driver_console_print(commandName);
+		driver_console_println("'. Usage:");
+		shell_printSynopsis(commandPattern);
+		driver_console_println("");
+		return;
+	}
+	for (int i=0; i<commandPattern->fixedArgumentCount; i++) {
+		const shell_ArgumentPattern *argumentPattern = (commandPattern->fixedArguments + i);
+		const shell_ArgumentType *type = argumentPattern->type;
+		if (!type->parser(segments[1 + i], parsedArguments + i)) {
+			driver_console_formatln("syntax error in argument %d: expected %s. Usage:", (i + 2), type->name);
+			shell_printSynopsis(commandPattern);
+			driver_console_println("");
+			return;
+		}
+	}
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// command invocation API
+// --------------------------------------------------------------------------------------------------------------------
+
+void shell_executeCommandLine(char *commandLine) {
+
+    // split into segments
+    if (!splitSegments(commandLine)) {
+        return;
+    }
+
+	// skip empty lines
+	if (segmentCount == 0) {
+		return;
+	}
+
+	// show the command to execute
+	echoSplitCommandLine();
+
+	// find a matching command
+	const char *commandName = segments[0];
+	commandPattern = NULL;
+	for (int i=0; i<shell_commandPatternCount; i++) {
+		const shell_CommandPattern *tryCommandPattern = shell_commandPatterns + i;
+		if (strcmp(commandName, tryCommandPattern->name) == 0) {
+			commandPattern = tryCommandPattern;
+			break;
+		}
+	}
+	if (commandPattern == NULL) {
+		driver_console_print("unknown command: ");
+		driver_console_println(commandName);
+		return;
+	}
+
+	// execute the command's callback
+	commandPattern->callback();
+
+}
+
 
 // TODO
 // TODO
