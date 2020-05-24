f1c17023474e155ce8cb1dd977bd27e94dca971e
Martin Geisse <martingeisse@googlemail.com>
2020-05-24 07:26:31 +0200

    added LAN test stub

---
+++ src/heos/c/driver-fpga/lan.c
@@ -0,0 +1,8 @@
+
+#include "lan.h"
+#include "../driver/terminal.h"
+
+void lanTest(void) {
+    driver_terminal_printlnString("--- begin LAN test ---");
+    driver_terminal_printlnString("--- LAN test finished ---");
+}
---
+++ src/heos/c/driver-fpga/lan.h
@@ -0,0 +1,7 @@
+
+#ifndef __DRIVER_LAN_H__
+#define __DRIVER_LAN_H__
+
+void lanTest(void);
+
+#endif
