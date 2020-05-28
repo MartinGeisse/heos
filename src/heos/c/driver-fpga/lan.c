ca9966d3ea700d62f211f2414933049a1dd6799a
Martin Geisse <martingeisse@googlemail.com>
2020-05-28 12:30:20 +0200

    LAN send test, WIP

--- src/heos/c/driver-fpga/lan.c
+++ src/heos/c/driver-fpga/lan.c
@@ -4,6 +4,10 @@
 #include "cpu.h"
 #include "keyboard.h"
 
+const unsigned char myMacAddress[6] = {
+    0x12, 0x34, 0x56, 0xfe, 0xdc, 0xba
+};
+
 static int mdcCycle(int outputBit) {
     outputBit = outputBit & 1;
     volatile int *clock = (int*)0x07000000;
@@ -76,8 +80,63 @@ static void printHexDigit(int digit) {
     driver_terminal_printChar(digit < 10 ? (digit + '0') : (digit - 10 + 'a'));
 }
 
+static unsigned int crc(unsigned char *buffer, unsigned int length) {
+   unsigned int crc = 0xffffffff;
+   for (unsigned int i = 0; i < length; i++) {
+      unsigned int byte = buffer[i];
+      crc = crc ^ byte;
+      for (int j = 0; j < 8; j++) {
+         unsigned int mask = -(crc & 1);
+         crc = (crc >> 1) ^ (0xedb88320 & mask);
+      }
+   }
+   return ~crc;
+}
+
+void sendPacket(unsigned char *destinationMacAddress, unsigned short etherType, int length, void *untypedData) {
+    if (length < 0 || length > 1500) {
+        driver_terminal_printString("ERROR: trying to send ethernet frame with invalid length: ");
+        driver_terminal_printlnInt(length);
+        return;
+    }
+    if (etherType < 1536) {
+        etherType = length;
+    }
+    unsigned char *data = (unsigned char *)untypedData;
+
+    volatile unsigned int *interface = (volatile unsigned int *)0x08000000;
+    interface[1024] = 0x55555555;
+    interface[1025] = 0xd5555555;
+    volatile unsigned char *packet = ((unsigned char *)(interface + 1026));
+    for (int i = 0; i < 6; i++) {
+        packet[i] = myMacAddress[i];
+        packet[6 + i] = destinationMacAddress[i];
+    }
+    packet[12] = etherType >> 8;
+    packet[13] = etherType & 0xff;
+    for (int i = 0; i < length; i++) {
+        packet[14 + i] = data[i];
+    }
+    unsigned int crcValue = crc(data, length);
+    packet[14 + length] = (crcValue & 0xff);
+    packet[15 + length] = (crcValue >> 8) & 0xff;
+    packet[16 + length] = (crcValue >> 16) & 0xff;
+    packet[17 + length] = (crcValue >> 24);
+    interface[3] = 18 + length;
+}
+
+unsigned char testPayload[] = {
+    0x44, 0x88, 0xaa, 0xcc
+};
+unsigned char testDestinationAddress[] = {
+    0xe0, 0x28, 0x6d, 0x04, 0xc7, 0xeb
+};
+
 void lanTest(void) {
     driver_terminal_printlnString("--- begin LAN test ---");
+
+    // send test packet
+    sendPacket(testDestinationAddress, 0x8abc, 4, testPayload);
 
     volatile unsigned int *interface = (volatile unsigned int *)0x08000000;
     while (1) {
@@ -92,6 +151,7 @@ void lanTest(void) {
             continue;
         }
         unsigned char *packet = ((unsigned char *)(interface + 1026));
+        packetLength -= 8;
 
         // ignore short packets -- must contain at least source/destination address and ethertype
         if (packetLength < 14) {
@@ -111,8 +171,8 @@ void lanTest(void) {
         driver_terminal_printlnUnsignedHexInt(packetLength);
 
         // print first 128 bytes
-        if (packetLength > 128) {
-            packetLength = 128;
+        if (packetLength > 256) {
+            packetLength = 256;
         }
         for (unsigned int i = 0; i < packetLength; i++) {
             unsigned char byte = packet[i];
@@ -121,6 +181,18 @@ void lanTest(void) {
             driver_terminal_printString("  ");
         }
         driver_terminal_println();
+
+        // CRC validation TODO
+        driver_terminal_printString("CRC: ");
+        driver_terminal_printlnUnsignedHexInt(crc(packet, packetLength - 4));
+
+        // CRC validation TODO
+        {
+            unsigned int computedCrc = crc(packet, packetLength - 4);
+            unsigned int specifiedCrc = packet[packetLength - 4] + (packet[packetLength - 3] << 8)
+                + (packet[packetLength - 2] << 16) + (packet[packetLength - 1] << 24);
+            driver_terminal_printlnString((computedCrc == specifiedCrc) ? "CRC ok" : "INVALID CRC!");
+        }
 
         // wait for keypress
         while (!KEY_STATE(0x5a));
